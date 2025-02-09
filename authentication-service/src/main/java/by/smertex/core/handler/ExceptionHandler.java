f6f823498dc0ea2ce943106b974eb7694adb1931
smertex <kiratsvetkova102@gmail.com>
2025-02-09 22:56:13 +0300

    Добавление обработчика

--- authentication-service/src/main/java/by/smertex/core/handler/ExceptionHandler.java
+++ authentication-service/src/main/java/by/smertex/core/handler/ExceptionHandler.java
@@ -5,10 +5,20 @@ import by.smertex.core.exception.AccessException;
 import by.smertex.core.exception.HideEmailDecryptException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionHandler {
+    @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
+    ResponseEntity<ResponseException> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(ResponseException.builder()
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .message(e.getMessage())
+                        .build());
+    }
+
     @org.springframework.web.bind.annotation.ExceptionHandler(AccessException.class)
     ResponseEntity<ResponseException> responseExceptionHandler(AccessException e) {
         return ResponseEntity.badRequest()
