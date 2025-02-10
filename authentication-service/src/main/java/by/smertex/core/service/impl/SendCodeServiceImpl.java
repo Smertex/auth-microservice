8b5f51c6371ef62b511ee818b12eef9e97fa167e
smertex <kiratsvetkova102@gmail.com>
2025-02-10 18:56:32 +0300

    Добавление self-injection и оптимизация зависимостейÐ

--- authentication-service/src/main/java/by/smertex/core/service/impl/SendCodeServiceImpl.java
+++ authentication-service/src/main/java/by/smertex/core/service/impl/SendCodeServiceImpl.java
@@ -7,8 +7,10 @@ import by.smertex.core.mapper.Mapper;
 import by.smertex.core.service.SendCodeService;
 import by.smertex.core.util.Cryptographer;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @Service
@@ -21,9 +23,12 @@ public class SendCodeServiceImpl implements SendCodeService {
 
     private final Cryptographer cryptographer;
 
+    @Lazy
+    private SendCodeService sendCodeService;
+
     @Override
     public String redirect(SecurityUserDto securityUserDto) {
-        sendCode(securityUserDto);
+        sendCodeService.sendCode(securityUserDto);
         return ServletUriComponentsBuilder.fromCurrentContextPath()
                 .path("/security/{secret}")
                 .buildAndExpand(cryptographer.encrypt(
@@ -33,6 +38,7 @@ public class SendCodeServiceImpl implements SendCodeService {
     }
 
     @Override
+    @Transactional
     public void sendCode(SecurityUserDto dto) {
         kafkaTemplate.send("email-notification-event-topic", null, emailNotificationEventSecurityUserDtoMapper.map(dto))
                 .exceptionally(exception -> {
--- email-sender-service/src/main/java/by/smertex/core/service/impl/SendCodeServiceImpl.java
+++ email-sender-service/src/main/java/by/smertex/core/service/impl/SendCodeServiceImpl.java
@@ -7,8 +7,10 @@ import by.smertex.core.service.SendCodeService;
 import by.smertex.core.util.CodeGenerator;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -19,14 +21,18 @@ public class SendCodeServiceImpl implements SendCodeService {
 
     private final CodeGenerator codeGenerator;
 
+    @Lazy
+    private SendCodeService sendCodeService;
+
     @Override
     public void sendToEmail(EmailNotificationEvent event) {
         String code = codeGenerator.generate();
         log.info("Sending {} code {}", event.email(), code);
-        sendToSecurityService(new EmailCodeGenerateEvent(event.email(), code));
+        sendCodeService.sendToSecurityService(new EmailCodeGenerateEvent(event.email(), code));
     }
 
     @Override
+    @Transactional
     public void sendToSecurityService(EmailCodeGenerateEvent event) {
         kafkaTemplate.send("email-code-generate-events-topic", null, event)
                 .exceptionally(exception -> {
